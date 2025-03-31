551257703f316f604a08ed204adf0d3e980f553c
andreadelaroca <asodelaroca@uamv.edu.ni>
2025-03-31 12:22:04 -0600

    Ejercicio Python 1

---
+++ ejemplo.py
@@ -0,0 +1,9 @@
+"""
+Calcular la nota de un estudiante, qu tiene 2 sistematicos de 30 y uno de 40
+"""
+
+sistematico1=int(input("Ingrese primer sistemático (max: 30): "))
+sistematico2=int(input("Ingrese segundo sistemático (max: 30): "))
+sistematico3=int(input("Ingrese tercer sistemático (max: 40): "))
+nota=(sistematico1+sistematico2+sistematico3)
+print("La nota del estudiante es: ", nota)
