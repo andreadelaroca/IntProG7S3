35679a3624f43a52bc857fc112e2a486b0894682
andreadelaroca <asodelaroca@uamv.edu.ni>
2025-04-02 14:08:38 -0600

    Cálculo del salario bruto de un empleado

---
+++ ejemplo4.py
@@ -0,0 +1,11 @@
+"""Cálculo del salario neto de un empleado"""
+
+salarbrut=float(input("Ingrese el salario bruto del empleado: "))
+imprenta=salarbrut*0.1
+segurosoc=salarbrut*0.05
+fondopen=salarbrut*0.03
+desctotal=(imprenta+segurosoc+fondopen)
+salarneto=salarbrut-desctotal
+print("El salario bruto es: ", salarbrut)
+print("El descuento total es: ", desctotal)
+print("El salario neto es: ", salarneto)
