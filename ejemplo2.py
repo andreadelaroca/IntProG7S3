ce35887eb1d137923ae79e6df5d7dcb4e5a0680f
andreadelaroca <asodelaroca@uamv.edu.ni>
2025-04-02 14:18:57 -0600

    Perímetro de un rectángulo

---
+++ ejemplo2.py
@@ -0,0 +1,8 @@
+"""Perímetro de un rectángulo"""
+
+b=float(input("Ingrese la base del rectángulo: "))
+h=float(input("Ingrese la altura del rectángulo: "))
+area=b*h
+perimetro=float(2*(b+h))
+print("El área del rectángulo es", area)
+print("El perimetro del rectángulo es", perimetro)
